439171456c02cdbcc1042e6749bc8152c8ff6b5d
Andrew Bankson <apbankson@gmail.com>
2020-02-10 22:14:44 -0500

    Starting on the JS search function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 lightbox.option({
     'showImageNumberLabel': false,
     'wrapAround': true,
-    // 'maxWidth': 600,
-    // 'maxHeight': 600
 })
+
+// Key up event listener on input that logs out value of input 
+$('#main-input').keyup(function() {
+    var searchString = $('#main-input').val().toLowerCase();
+    console.log(searchString);
+});
+
+// Targeting the captions
+let captions = $('div.container').children();
+
+console.log(captions);
+
+for (let i = 0; i < captions.length; i++) {
+    let cap = $(captions[i]).attr('data-title');
+    cap = cap.toLowerCase();
+    console.log(cap);
+    let bool = cap.includes(searchString);
+    console.log(bool);
+}
