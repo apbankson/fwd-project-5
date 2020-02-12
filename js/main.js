303c03336d86cd2e546aa4b43fef2581efc6a7a8
Andrew Bankson <apbankson@gmail.com>
2020-02-11 19:37:25 -0500

    Added search function and media query for layout

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
+// Plugin configuration
+
 lightbox.option({
     'showImageNumberLabel': false,
     'wrapAround': true,
 })
 
 // Key up event listener on input that logs out value of input 
-$('#main-input').keyup(function() {
-    var searchString = $('#main-input').val().toLowerCase();
-    console.log(searchString);
-});
 
-// Targeting the captions
 let captions = $('div.container').children();
 
-console.log(captions);
+$('#main-input').keyup(function() {
+    let searchString = $('#main-input').val().toLowerCase();
+    for (let i = 0; i < captions.length; i++) {
+        let cap = $(captions[i]).attr('data-title');
+        cap = cap.toLowerCase();
+        let match = cap.includes(searchString);
+        if (!match) {
+            $(captions[i]).addClass('hide');
+        } else if (match) {
+            $(captions[i]).removeClass('hide');
+        }
+    };
+});
 
-for (let i = 0; i < captions.length; i++) {
-    let cap = $(captions[i]).attr('data-title');
-    cap = cap.toLowerCase();
-    console.log(cap);
-    let bool = cap.includes(searchString);
-    console.log(bool);
-}
+
